d102b58d9191afab46b99c5779e080a4664b05d2
Jared Martínez <jaredartt@gmail.com>
2023-09-12 17:41:15 +0200

    added controllers folder

---
+++ controllers/homeController.js
@@ -0,0 +1,6 @@
+// controllers/homeController.js
+function displayHome(req, res) {
+  res.send('Name: Jared Martinez');
+}
+
+module.exports = { displayHome };
--- server.js
+++ server.js
@@ -2,9 +2,10 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Name: Ruben Martinez.'); 
-});
+// Require the controller
+const homeController = require('./controllers/homeController');
+// Define routes and use the controller function
+app.get('/', homeController.displayHome);
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
