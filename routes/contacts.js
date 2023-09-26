dcb693075c174201704cb90fa510bc4ceec9f960
Jared Martínez <jaredartt@gmail.com>
2023-09-26 11:36:59 +0200

    deleted a comment

--- routes/contacts.js
+++ routes/contacts.js
@@ -32,19 +32,3 @@ router.get('/:id', async (req, res) => {
 // Other routes (POST, PUT, DELETE) can be defined here
 
 module.exports = router;
-
-
-
-
-
-
-// // We can import routes here thanks to the Express function Router()
-// const routes = require('express').Router();
-// const controller = require('../controllers/homeController');
-
-// routes.get('/', controller.normalRoute);
-// routes.get('/jared', controller.JaredRoute);
-// routes.get('/ruben', controller.RubenRoute);
-// routes.get('/pedro', controller.PedroRoute);
-
-// module.exports = routes;
