1fdae44714b8511d617d541ddc502685008b9c8f
Jared Martínez <jaredartt@gmail.com>
2023-09-26 11:44:23 +0200

    file will stay as server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app
     res.setHeader('Access-Control-Allow-Origin', '*');
     next();
   })
-  .use('/', require('./routes/'));
+  .use('/', require('./routes'));
  
 mongodb.initDb((err, mongodb) => {
   if (err) {
