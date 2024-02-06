da5a02e028c7c03dd00fc611a7c04da85a4e15d4
Jared Martínez <jaredartt@gmail.com>
2024-02-06 13:12:48 +0100

    Added 'schemes' in the swagger.js file

--- swagger.js
+++ swagger.js
@@ -5,13 +5,11 @@ const doc = {
     title: 'Task Manager API',
     description: "Let's get your work done!"
   },
-  host: 'taskmanager-cega.onrender.com'
+  host: 'taskmanager-cega.onrender.com',
+  schemes: ['https'],
 };
 
 const outputFile = './swagger-output.json';
 const routes = ['./routes/index.js'];
-
-/* NOTE: If you are using the express Router, you must pass in the 'routes' only the 
-root file where the route starts, such as index.js, app.js, routes.js, etc ... */
 
 swaggerAutogen(outputFile, routes, doc);
